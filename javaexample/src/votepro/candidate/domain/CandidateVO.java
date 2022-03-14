58edb2abf96da49d46dd2aba2bbba92b5294e981
CodingWon <kingcoding2298@gmail.com>
2022-03-15 06:26:38 +0900

    votepro : CandidateVO

--- javaexample/src/votepro/candidate/domain/CandidateVO.java
+++ javaexample/src/votepro/candidate/domain/CandidateVO.java
@@ -1,4 +1,14 @@
 package votepro.candidate.domain;
 
 public class CandidateVO {
+    private Integer cno;
+    private String name;
+    private String party;
+
+    public CandidateVO(Integer cno, String name, String party) {
+        this.cno = cno;
+        this.name = name;
+        this.party = party;
+
+    }
 }
