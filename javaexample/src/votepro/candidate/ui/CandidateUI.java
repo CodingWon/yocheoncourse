2ebeaa10cdf65250c77508adad7926ad2ff22d86
CodingWon <kingcoding2298@gmail.com>
2022-03-15 06:52:55 +0900

    votepro : CandidateUI interface implement

--- javaexample/src/votepro/candidate/ui/CandidateUI.java
+++ javaexample/src/votepro/candidate/ui/CandidateUI.java
@@ -2,16 +2,17 @@ package votepro.candidate.ui;
 
 import votepro.candidate.domain.CandidateVO;
 import votepro.candidate.service.CandidateService;
+import votepro.ui.BasicUI;
 
 import java.util.Arrays;
 
-public class CandidateUI {
+public class CandidateUI implements BasicUI {
     CandidateService candidateService;
 
     public CandidateUI(CandidateService candidateService){
         this.candidateService = candidateService;
     }
-
+    @Override
     public void display(){
         CandidateVO[] candidates = candidateService.showCandidate();
         Arrays.stream(candidates).forEach(vo -> System.out.println("기호"+vo.getCno() +"번 " + vo.getName() + "," + vo.getParty()));
