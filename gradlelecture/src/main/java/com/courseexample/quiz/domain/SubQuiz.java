d968bf15ca94dbdf7592e1859fd58cd9d4d1230a
CodingWon <kingcoding2298@gmail.com>
2022-03-19 19:10:44 +0900

    퀴즈 data 이용

---
+++ gradlelecture/src/main/java/com/courseexample/quiz/domain/SubQuiz.java
@@ -0,0 +1,8 @@
+package com.courseexample.quiz.domain;
+
+public class SubQuiz extends Quiz{
+    @Override
+    public String getDesc() {
+        return "주관식 입니다.";
+    }
+}
---
+++ gradlelecture/src/main/java/com/courseexample/quiz/repository/MyRepository.java
@@ -0,0 +1,59 @@
+package com.courseexample.quiz.repository;
+
+import com.courseexample.quiz.domain.MultipleQuiz;
+import com.courseexample.quiz.domain.OXQuiz;
+import com.courseexample.quiz.domain.Quiz;
+import com.courseexample.quiz.domain.SubQuiz;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MyRepository implements QuizRepository{
+
+    private Quiz[] quizzes;
+
+    public MyRepository() throws FileNotFoundException {
+        File file = new File("C:\\Users\\JIwon\\IdeaProjects\\yocheoncourse\\gradlelecture\\src\\main\\java\\com\\courseexample\\quiz\\data\\quizdata.txt");
+        Scanner sc = new Scanner(file);
+        quizzes = new Quiz[6];
+
+        for(int i =0; i<quizzes.length; i++){
+            String line = sc.nextLine();
+            String[] arr = line.split(",");
+
+            String title = arr[1];
+            String answer = arr[2];
+
+            if(arr[0].equals("OX")){
+                OXQuiz oxQuiz = new OXQuiz();
+                quizzes[i] = oxQuiz;
+            } else if(arr[0].equals("SUB")){
+                SubQuiz subQuiz = new SubQuiz();
+                quizzes[i] = subQuiz;
+            }else if(arr[0].equals("MUL")){
+                MultipleQuiz multipleQuiz = new MultipleQuiz();
+                quizzes[i] = multipleQuiz;
+            }
+
+            quizzes[i].setTitle(title);
+            quizzes[i].setAnswer(answer);
+
+            if(arr.length>=4){
+                String[] examples = new String[3];
+                examples[0] = arr[3];
+                examples[1] = arr[4];
+                examples[2] = arr[5];
+                quizzes[i].setArr(examples);
+            }
+        }
+
+    }
+
+    @Override
+    public Quiz[] getQuiz() {
+        return quizzes;
+    }
+
+
+}
