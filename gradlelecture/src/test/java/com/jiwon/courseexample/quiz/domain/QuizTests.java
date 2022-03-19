ed3edabfbcb67dc6adf73392fd7b1f69067c17ff
CodingWon <kingcoding2298@gmail.com>
2022-03-19 16:53:12 +0900

    QuizTests

--- gradlelecture/src/test/java/com/jiwon/courseexample/quiz/domain/QuizTests.java
+++ gradlelecture/src/test/java/com/jiwon/courseexample/quiz/domain/QuizTests.java
@@ -1,8 +1,12 @@
 package com.jiwon.courseexample.quiz.domain;
 
+import com.courseexample.quiz.domain.MultipleQuiz;
 import com.courseexample.quiz.domain.OXQuiz;
 import com.courseexample.quiz.domain.Quiz;
+import com.jiwon.c8_0315.quiz.domain.SubQuiz;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 public class QuizTests {
 
@@ -13,5 +17,16 @@ public class QuizTests {
         quiz.setAnswer("O");
         System.out.println(quiz.getTitle());
         quiz.getDesc();
+    }
+
+    @Test
+    public void test2(){
+        MultipleQuiz multipleQuiz = new MultipleQuiz();
+        multipleQuiz.setTitle("임진왜란?");
+        multipleQuiz.setArr(new String[]{"1422","2233"});
+        multipleQuiz.setAnswer("1");
+        System.out.println(multipleQuiz.getTitle());
+        System.out.println(Arrays.toString(multipleQuiz.getArr()));
+        System.out.println(multipleQuiz.checkAnswer("1422"));
     }
 }
